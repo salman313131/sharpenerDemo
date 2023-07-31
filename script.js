532e049d427892a2514837a6aab23853df285b66
salman <salmankhan.faze@gmail.com>
2023-07-31 16:59:49 +0530

    filter

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const items = document.getElementById('items')
 
 const form = document.getElementById('addForm')
+const filter = document.getElementById('filter')
+
+const newInput = document.createElement('input')
+newInput.className='form-control mr-2'
+newInput.id='item1'
+newInput.setAttribute('type','text')
+var preInput = document.getElementById('sub')
+form.insertBefore(newInput,preInput)
+
+filter.addEventListener('keyup',filterItems)
 
 form.addEventListener('submit',addInput)
 items.addEventListener('click',removeItem)
@@ -17,13 +27,18 @@ for(let i=0;i<itemsList.length;i++){
 function addInput(e){
     e.preventDefault();
     var inputItem = document.getElementById('item').value;
+    var newInputItem = document.getElementById('item1').value;
     const newLi = document.createElement('li');
     newLi.className = 'list-group-item';
-    newLi.appendChild(document.createTextNode(inputItem));
+    newLi.appendChild(document.createTextNode(inputItem+" "+newInputItem));
     const newBtn = document.createElement('button');
     newBtn.className = 'btn btn-danger btn-sm float-right delete';
     newBtn.appendChild(document.createTextNode('X'));
+    const editButton = document.createElement('button');
+    editButton.appendChild(document.createTextNode('edit'))
+    editButton.className='btn btn-primary btn-sm float-right'
     newLi.appendChild(newBtn);
+    newLi.appendChild(editButton);
     items.appendChild(newLi);
 }
 function removeItem(e){
@@ -33,4 +48,17 @@ function removeItem(e){
            items.removeChild(li);
         }
     }
+}
+function filterItems(e){
+    var text = e.target.value.toLowerCase();
+    var itmList = document.getElementsByClassName('list-group-item')
+    Array.from(itmList).forEach(function(item){
+        var itemText = item.firstChild.textContent;
+        if(itemText.toLowerCase().indexOf(text)!=-1){
+            item.style.display = 'block'
+        }
+        else{
+            item.style.display = 'none'
+        }
+    })
 }
